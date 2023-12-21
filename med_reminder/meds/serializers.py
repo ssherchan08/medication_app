004f27b6b228ddfb6240febf2f3c5e8dba8d68e6
Sumnima Sherchan <sumnimasherchan@bajratechnologies.com>
2023-12-21 20:48:08 +0545

    Change time format

--- med_reminder/meds/serializers.py
+++ med_reminder/meds/serializers.py
@@ -5,6 +5,7 @@ class MedicineSerializer(serializers.ModelSerializer):
 
     reminder_days = fields.MultipleChoiceField(choices=DAYS_OF_WEEK)
     taken_on = fields.MultipleChoiceField(choices=DAYS_OF_WEEK)
+    reminder = serializers.TimeField(format='%I:%M %p', input_formats='%I:%M %p')
 
     class Meta:
         model = Medicine
